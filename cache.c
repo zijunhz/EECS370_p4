48a444124cc44381f733eefd8fd705bc1eb0a335
Zijun Yang <zijunhz@126.com>
2023-11-03 14:10:31 -0400

    seems to have finished the project

--- cache.c
+++ cache.c
@@ -14,7 +14,6 @@
 // Powers of 2 have exactly one 1 and the rest 0's, and 0 isn't a power of 2.
 #define is_power_of_2(val) (val && !(val & (val - 1)))
 
-
 /*
  * Accesses 1 word of memory.
  * addr is a 16-bit LC2K word address.
@@ -30,27 +29,19 @@ extern int mem_access(int addr, int write_flag, int write_data);
  */
 extern int get_num_mem_accesses(void);
 
-//Use this when calling printAction. Do not modify the enumerated type below.
-enum actionType
-{
-    cacheToProcessor,
-    processorToCache,
-    memoryToCache,
-    cacheToMemory,
-    cacheToNowhere
-};
+// Use this when calling printAction. Do not modify the enumerated type below.
+enum actionType { cacheToProcessor, processorToCache, memoryToCache, cacheToMemory, cacheToNowhere };
 
 /* You may add or remove variables from these structs */
-typedef struct blockStruct
-{
+typedef struct blockStruct {
+    int valid;
     int data[MAX_BLOCK_SIZE];
     int dirty;
     int lruLabel;
     int tag;
 } blockStruct;
 
-typedef struct cacheStruct
-{
+typedef struct cacheStruct {
     blockStruct blocks[MAX_CACHE_SIZE];
     int blockSize;
     int numSets;
@@ -63,12 +54,15 @@ cacheStruct cache;
 void printAction(int, int, enum actionType);
 void printCache(void);
 
+blockStruct* getBlock(int set, int block) {
+    return &(cache.blocks[set * cache.blocksPerSet + block]);
+}
+
 /*
  * Set up the cache with given command line parameters. This is
  * called once in main(). You must implement this function.
  */
-void cache_init(int blockSize, int numSets, int blocksPerSet)
-{
+void cache_init(int blockSize, int numSets, int blocksPerSet) {
     if (blockSize <= 0 || numSets <= 0 || blocksPerSet <= 0) {
         printf("error: input parameters must be positive numbers\n");
         exit(1);
@@ -87,14 +81,38 @@ void cache_init(int blockSize, int numSets, int blocksPerSet)
     if (!is_power_of_2(numSets)) {
         printf("warning: numSets %d is not a power of 2\n", numSets);
     }
-    printf("Simulating a cache with %d total lines; each line has %d words\n",
-        numSets * blocksPerSet, blockSize);
-    printf("Each set in the cache contains %d lines; there are %d sets\n",
-        blocksPerSet, numSets);
+    printf("Simulating a cache with %d total lines; each line has %d words\n", numSets * blocksPerSet, blockSize);
+    printf("Each set in the cache contains %d lines; there are %d sets\n", blocksPerSet, numSets);
 
     // Your code here
-
+    cache.numSets = numSets;
+    cache.blockSize = blockSize;
+    cache.blocksPerSet = blocksPerSet;
+    for (int i = 0; i < numSets; ++i)
+        for (int j = 0; j < blocksPerSet; ++j) {
+            blockStruct* theBlock = getBlock(i, j);
+            theBlock->dirty = 0;
+            theBlock->lruLabel = j;
+            theBlock->valid = 0;
+            theBlock->tag = 0;
+        }
     return;
+}
+
+void adjustLru(int set, int updatedBlock) {
+    blockStruct* theBlock = getBlock(set, updatedBlock);
+    int x = theBlock->lruLabel;  // adjust lru
+    theBlock->lruLabel = cache.blocksPerSet - 1;
+    for (int jj = 0; jj < cache.blocksPerSet; ++jj) {
+        blockStruct* theTempBlock = getBlock(set, jj);
+        if (theTempBlock != theBlock && theTempBlock->lruLabel > x)
+            theTempBlock->lruLabel--;
+    }
+    // printf("lru label for set %d:\n", set);
+    // for (int j = 0; j < cache.blocksPerSet; ++j) {
+    //     printf("%d ", getBlock(set, j)->lruLabel);
+    // }
+    // printf("\n");
 }
 
 /*
@@ -107,14 +125,91 @@ void cache_init(int blockSize, int numSets, int blocksPerSet)
  * The return of mem_access is undefined if write_flag is 1.
  * Thus the return of cache_access is undefined if write_flag is 1.
  */
-int cache_access(int addr, int write_flag, int write_data)
-{
+int cache_access(int addr, int write_flag, int write_data) {
     /* The next line is a placeholder to connect the simulator to
     memory with no cache. You will remove this line and implement
     a cache which interfaces between the simulator and memory. */
-    return mem_access(addr, write_flag, write_data);
-}
+    // printf("================ %d %d %d\n", addr, write_flag, write_data);
+    int targetSet = (addr / cache.blockSize) % cache.numSets;
+    int targetTag = addr / (cache.blockSize * cache.blocksPerSet);
+    int targetLine = addr % cache.blockSize;
+    int addrStart = targetTag * cache.blocksPerSet * cache.blockSize;
+    if (write_flag) {  // writes
+        for (int j = 0; j < cache.blocksPerSet; ++j) {
+            blockStruct* theBlock = getBlock(targetSet, j);
+            if (theBlock->valid && targetTag == theBlock->tag) {  // hit
+                adjustLru(targetSet, j);
+                printAction(addr, 1, processorToCache);
+                theBlock->dirty = 1;
+                theBlock->data[targetLine] = write_data;
+                return 0;
+            }
+        }
+        // miss
+        for (int j = 0; j < cache.blocksPerSet; ++j) {
+            blockStruct* theBlock = getBlock(targetSet, j);
+            if (theBlock->lruLabel == 0) {
+                if (theBlock->valid && theBlock->dirty) {  // about to overwrite! write back to mem
+                    int oldAddrStart = theBlock->tag * cache.blocksPerSet * cache.blockSize;
+                    printAction(oldAddrStart, cache.blockSize, cacheToMemory);
+                    for (int k = 0; k < cache.blockSize; ++k) {
+                        mem_access(oldAddrStart + k, 1, theBlock->data[k]);
+                    }
+                } else if (theBlock->valid) {
+                    int oldAddrStart = theBlock->tag * cache.blocksPerSet * cache.blockSize;
+                    printAction(oldAddrStart, cache.blockSize, cacheToNowhere);
+                }
 
+                theBlock->dirty = 1;
+                theBlock->valid = 1;
+                theBlock->tag = targetTag;
+                adjustLru(targetSet, j);
+                printAction(addrStart, cache.blockSize, memoryToCache);
+                for (int k = 0; k < cache.blockSize; ++k) {
+                    theBlock->data[k] = mem_access(addrStart + k, 0, 0);
+                }
+                printAction(addr, 1, processorToCache);
+                theBlock->data[targetLine] = write_data;
+                return 0;
+            }
+        }
+    } else {  // reads
+        for (int j = 0; j < cache.blocksPerSet; ++j) {
+            blockStruct* theBlock = getBlock(targetSet, j);
+            if (theBlock->valid && targetTag == theBlock->tag) {  // hit
+                adjustLru(targetSet, j);
+                printAction(addr, 1, cacheToProcessor);
+                return theBlock->data[targetLine];
+            }
+        }
+        // miss
+        for (int j = 0; j < cache.blocksPerSet; ++j) {
+            blockStruct* theBlock = getBlock(targetSet, j);
+            if (theBlock->lruLabel == 0) {                 // read from mem to cache
+                if (theBlock->valid && theBlock->dirty) {  // about to overwrite! write back to mem
+                    int oldAddrStart = theBlock->tag * cache.blocksPerSet * cache.blockSize;
+                    printAction(oldAddrStart, cache.blockSize, cacheToMemory);
+                    for (int k = 0; k < cache.blockSize; ++k) {
+                        mem_access(oldAddrStart + k, 1, theBlock->data[k]);
+                    }
+                } else if (theBlock->valid) {
+                    int oldAddrStart = theBlock->tag * cache.blocksPerSet * cache.blockSize;
+                    printAction(oldAddrStart, cache.blockSize, cacheToNowhere);
+                }
+                theBlock->tag = targetTag;
+                theBlock->dirty = 0;
+                theBlock->valid = 1;
+                adjustLru(targetSet, j);
+                printAction(addrStart, cache.blockSize, memoryToCache);
+                for (int k = 0; k < cache.blockSize; ++k) {
+                    theBlock->data[k] = mem_access(addrStart + k, 0, 0);
+                }
+                printAction(addr, 1, cacheToProcessor);
+                return theBlock->data[targetLine];
+            }
+        }
+    }
+}
 
 /*
  * print end of run statistics like in the spec. **This is not required**,
@@ -123,8 +218,7 @@ int cache_access(int addr, int write_flag, int write_data)
  * DO NOT delete this function, or else it won't compile.
  * DO NOT print $$$ in this function
  */
-void printStats(void)
-{
+void printStats(void) {
     return;
 }
 
@@ -141,30 +235,23 @@ void printStats(void)
  *  -    cacheToMemory: evicting cache data and writing it to the memory
  *  -    cacheToNowhere: evicting cache data and throwing it away
  */
-void printAction(int address, int size, enum actionType type)
-{
+void printAction(int address, int size, enum actionType type) {
     printf("$$$ transferring word [%d-%d] ", address, address + size - 1);
 
     if (type == cacheToProcessor) {
         printf("from the cache to the processor\n");
-    }
-    else if (type == processorToCache) {
+    } else if (type == processorToCache) {
         printf("from the processor to the cache\n");
-    }
-    else if (type == memoryToCache) {
+    } else if (type == memoryToCache) {
         printf("from the memory to the cache\n");
-    }
-    else if (type == cacheToMemory) {
+    } else if (type == cacheToMemory) {
         printf("from the cache to the memory\n");
-    }
-    else if (type == cacheToNowhere) {
+    } else if (type == cacheToNowhere) {
         printf("from the cache to nowhere\n");
-    }
-    else {
+    } else {
         printf("Error: unrecognized action\n");
         exit(1);
     }
-
 }
 
 /*
@@ -172,8 +259,7 @@ void printAction(int address, int size, enum actionType type)
  * This is for debugging only and is not graded, so you may
  * modify it, but that is not recommended.
  */
-void printCache(void)
-{
+void printCache(void) {
     printf("\ncache:\n");
     for (int set = 0; set < cache.numSets; ++set) {
         printf("\tset %i:\n", set);
--- p4spec.4.2.1.as
+++ p4spec.4.2.1.as
@@ -1,4 +1,4 @@
-	sw	0	1	6
+	sw	0	1	6	mem[ 6 + reg[ 0 ] ] = 1
 	lw	0	1	23
 	lw	0	1	30
 	halt
